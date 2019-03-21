085243d851607a27c86559f445dbf3f8c219f050
skasai5296 <seito5296@gmail.com>
2019-03-21 13:46:23 +0900

    done with debugging yolov3

--- code/models/imagemodels.py
+++ code/models/imagemodels.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import time
 
 # Bottleneck layer.
 class Bottleneck(nn.Module):
@@ -9,18 +10,18 @@ class Bottleneck(nn.Module):
         self.repeat = repeat
         self.model = nn.ModuleList()
         for i in range(self.repeat):
-            self.model.append(name='bottle{}'.format(i+1), nn.Sequential(
-                    nn.Conv2d(C, self.bottle_c, 1, stride=1, padding=1),
+            self.model.append(nn.Sequential(
+                    nn.Conv2d(C, self.bottle_c, 1, stride=1, padding=0),
                     nn.BatchNorm2d(self.bottle_c),
                     nn.LeakyReLU(),
                     nn.Conv2d(self.bottle_c, C, 3, stride=1, padding=1),
                     nn.BatchNorm2d(C),
-                    nn.LeakyReLU()
+                    nn.LeakyReLU())
                     )
 
     def forward(self, x):
         for layer in self.model:
-            x += layer(x)
+            x = layer(x)
         return x
 
 # Downsampling layer.
@@ -30,7 +31,7 @@ class Down(nn.Module):
         self.down_c = C*2
         self.model = nn.Sequential(
                 nn.Conv2d(C, self.down_c, 3, stride=2, padding=1),
-                nn.BatchNorm2d(self.bottle_c),
+                nn.BatchNorm2d(self.down_c),
                 nn.LeakyReLU()
                 )
 
@@ -38,23 +39,37 @@ class Down(nn.Module):
         return self.model(x)
 
 
+"""
+input : (bs, 3, imsize, imsize)
+output : (bs, B*(4+1+classnum), S, S)
+"""
 class YOLOv3(nn.Module):
-    def __init__(self, in_c, imsize=256, resnumbers=[1, 2, 8, 8, 4]):
-        super(YOLO, self).__init__()
-        # input : (bs, 3, H=256, W=256)
-        self.model = nn.Sequential()
+    def __init__(self, in_c=3, classnum=80, S=7, B=3, imsize=256, resnumbers=[1, 2, 8, 8, 4]):
+        super(YOLOv3, self).__init__()
+        self.model = nn.ModuleList()
         self.current_c = 32
-        self.model.add_module(name='conv1', nn.Conv2d(in_c, self.current_c, 3, stride=1, padding=1))
+        self.S = S
+        self.out_c = B*(4+1+classnum)
         self.repeat = resnumbers
         self.current_im = imsize
+
+        self.model.append(nn.Conv2d(in_c, self.current_c, 3, stride=1, padding=1))
         for i in range(len(self.repeat)):
-            self.model.add_module(name='down{}'.format(i+1), Down(self.current_c))
+            self.model.append(Down(self.current_c))
             self.current_c *= 2
-            self.model.add_module(name='bottle{}'.format(i+1), Bottleneck(self.current_c, repeat=self.repeat[i]))
-        self.model.add_module(nn.AvgPool2d())
+            self.current_im /= 2
+            self.current_im = int(self.current_im)
+            self.model.append(Bottleneck(self.current_c, repeat=self.repeat[i]))
+        self.model.append(nn.AvgPool2d(self.current_im))
+        self.model.append(nn.Linear(self.current_c, self.S*self.S*self.out_c))
 
     def forward(self, x):
-        return None
+        self.bs = x.size(0)
+        for layer in self.model[:-1]:
+            x = layer(x)
+        x = x.squeeze()
+        x = self.model[-1](x)
+        return x.view(self.bs, self.out_c, self.S, self.S)
 
 """
 Region Proposal Network
@@ -90,6 +105,24 @@ class RPN(nn.Module):
         self.model = []
 
     def forward(self, x):
-
-        
         return None
+
+
+'''
+for debugging.
+'''
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = YOLOv3()
+    if torch.cuda.device_count() > 1:
+        print('Using {} GPUs...'.format(torch.cuda.device_count()))
+        model = nn.DataParallel(model)
+    before = time.time()
+    for i in range(100):
+        input = torch.empty((64, 3, 256, 256)).to(device)
+        model = model.to(device)
+        out = model(input)
+    print('time per loop: {}s'.format((time.time()-before)/100))
+
+
+
